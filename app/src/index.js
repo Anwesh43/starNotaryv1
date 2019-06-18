364a780dbe109b7fb8bf949a1cf9fed7c0b0dc44
Anwesh43 <anweshthecool0@gmail.com>
2019-06-18 23:19:47 +0530

    adding front end

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,10 @@
 import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import starNotaryArtifact from "../../build/contracts/StarNotary.json";
 
 const App = {
   web3: null,
   account: null,
-  meta: null,
+  instance: null,
 
   start: async function() {
     const { web3 } = this;
@@ -12,47 +12,40 @@ const App = {
     try {
       // get contract instance
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
+      const deployedNetwork = starNotaryArtifact.networks[networkId];
+      this.instance = new web3.eth.Contract(
+        starNotaryArtifact.abi,
         deployedNetwork.address,
       );
 
+      console.log(this.instance)
       // get accounts
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
 
-      this.refreshBalance();
+      this.getInitialDetails();
     } catch (error) {
       console.error("Could not connect to contract or chain.");
     }
   },
 
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
-
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
+  getInitialDetails: async function() {
+      if (this.instance && this.instance.methods) {
+          console.log("coming here")
+          const name = await this.instance.methods.name().call()
+          const address = await this.instance.methods.owner().call()
+          console.log(name)
+          console.log(address)
+          document.getElementById('name').innerHTML = name
+          document.getElementById('address').innerHTML = address
+      }
   },
-
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
-
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
-  },
-
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
-  },
+  claimStar: async function() {
+      await this.instance.methods.claimStar().send({from : this.account})
+      const owner = await this.instance.methods.owner().call()
+      console.log(owner)
+      document.getElementById('address').innerHTML = owner
+  }
 };
 
 window.App = App;
@@ -64,11 +57,11 @@ window.addEventListener("load", function() {
     window.ethereum.enable(); // get permission to access accounts
   } else {
     console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",
+      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
     );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
+      new Web3.providers.HttpProvider("http://127.0.0.1:8545/"),
     );
   }
 
